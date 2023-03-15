aa689f3bdf1788d376e4b47706d9ccf9d4f68d65
Wendell Mccaulay Ramos Alves <wendellramos.gft@sulamerica.com.br>
2023-03-15 20:23:11 -0300

    Página não encontrada

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import About from '../../views/examples/About';
 import Home from '../../views/examples/Home';
 import Param from '../../views/examples/Param';
+import NotFound from '../../views/examples/NotFound';
 
 const Content = (props) => {
     return (
@@ -11,6 +12,7 @@ const Content = (props) => {
                 <Route path="/" element={<Home/>} />
                 <Route path="/about" element={<About/>} />
                 <Route path="/param/:id" element={<Param/>} />
+                <Route path="*" element={<NotFound/>} />
             </Routes>
         </main>
     );
